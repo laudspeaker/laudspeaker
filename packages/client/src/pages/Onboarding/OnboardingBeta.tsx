2a6becde9b5d93543abe6687d09e1cba44b1a191
Abheek Basu <abheek94@gmail.com>
2022-10-23 16:02:01 +0100

    better icon

--- packages/client/src/pages/Onboarding/OnboardingBeta.tsx
+++ packages/client/src/pages/Onboarding/OnboardingBeta.tsx
@@ -21,6 +21,21 @@
   }
   ```
 */
+import Header from "components/Header";
+import { ApiConfig } from "../../constants";
+import React, { useEffect, useLayoutEffect } from "react";
+import ApiService from "services/api.service";
+import Input from "../../components/Elements/Input";
+import Select from "../../components/Elements/Select";
+import { allChannels } from "../Settings/EventsProvider";
+import { useTypedSelector } from "hooks/useTypeSelector";
+import {
+  setDomainsList,
+  setSettingData,
+  setSettingsPrivateApiKey,
+} from "reducers/settings";
+import { useDispatch } from "react-redux";
+
 import { Fragment, useState } from "react";
 import { Dialog, Menu, Transition } from "@headlessui/react";
 import {
@@ -45,6 +60,8 @@ import {
   ChevronRightIcon,
   MagnifyingGlassIcon,
 } from "@heroicons/react/20/solid";
+import LaudspeakerIcon from "../../assets/images/laudspeakerIcon.svg";
+import LaudspeakerWhiteIcon from "../../assets/images/laudspeakerWhiteIcon.svg";
 
 const navigation = [
   { name: "Home", href: "#", icon: HomeIcon, current: true },
@@ -86,8 +103,141 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
+interface IntegrationsData {
+  sendingName: string;
+  sendingEmail: string;
+  slackId: string;
+  eventProvider: string;
+}
+
 export default function OnboardingBeta() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const { settings, domainsList } = useTypedSelector((state) => state.settings);
+  const [integrationsData, setIntegrationsData] = useState<IntegrationsData>({
+    sendingName: "",
+    sendingEmail: "",
+    slackId: "",
+    eventProvider: "posthog",
+  });
+  const dispatch = useDispatch();
+  const [slackInstallUrl, setSlackInstallUrl] = useState<string>("");
+  const [domainName, setDomainName] = useState<any>(settings.domainName || "");
+  const [domainList, setDomainList] = useState<any>(domainsList || []);
+  const [privateApiKey, setPrivateApiKey] = useState<string>(
+    settings.privateApiKey || ""
+  );
+
+  const handleInputSettingsData = (name: any, value: any): any => {
+    dispatch(setSettingData({ ...settings, [name]: value }));
+  };
+
+  const callDomains = async () => {
+    if (privateApiKey) {
+      dispatch(setSettingsPrivateApiKey(privateApiKey));
+      const response = await dispatch(setDomainsList(privateApiKey));
+      if (response?.data) {
+        setDomainList(response?.data);
+      }
+    }
+  };
+
+  useLayoutEffect(() => {
+    const func = async () => {
+      const { data } = await ApiService.get({
+        url: `${ApiConfig.slackInstall}`,
+      });
+      setSlackInstallUrl(data);
+    };
+    func();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      const { data } = await ApiService.get({ url: "/accounts" });
+      const { sendingName, sendingEmail, slackTeamId } = data;
+      setIntegrationsData({
+        ...integrationsData,
+        sendingName,
+        sendingEmail,
+        slackId: slackTeamId?.[0],
+      });
+    })();
+  }, []);
+
+  const handleIntegrationsDataChange = (e: any) => {
+    setIntegrationsData({
+      ...integrationsData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const parametersToConfigure: { [key: string]: React.ReactElement } = {
+    posthog: (
+      <form>
+        <Input
+          isRequired
+          value={settings.phPrivateApiKey}
+          label="Private API Key"
+          placeholder={"****  "}
+          name="name"
+          id="name"
+          className="w-full p-[15px_16px_15px_16px] bg-white border-[1px] border-[#D1D5DB] font-[Inter] text-[16px] mb-[10px]"
+          onChange={(e) => {
+            handleInputSettingsData("phPrivateApiKey", e.target.value);
+          }}
+        />
+        <Input
+          isRequired
+          value={settings.phProjectId}
+          label="Project Id"
+          placeholder={"****  "}
+          name="name"
+          id="name"
+          className="w-full p-[15px_16px_15px_16px] bg-white border-[1px] border-[#D1D5DB] font-[Inter] text-[16px] mb-[10px]"
+          onChange={(e) => {
+            handleInputSettingsData("phProjectId", e.target.value);
+          }}
+        />
+        <Input
+          isRequired
+          value={settings.phHostUrl}
+          label="Posthog Url"
+          placeholder={"https://app.posthog.com"}
+          name="name"
+          id="name"
+          className="w-full p-[15px_16px_15px_16px] bg-white border-[1px] border-[#D1D5DB] font-[Inter] text-[16px] mb-[10px]"
+          onChange={(e) => {
+            handleInputSettingsData("phHostUrl", e.target.value);
+          }}
+        />
+        <Input
+          isRequired
+          value={settings.phSms}
+          label="Name of SMS / Phone number field on your Posthog person"
+          placeholder={"$phoneNumber"}
+          name="name"
+          id="name"
+          className="w-full p-[15px_16px_15px_16px] bg-white border-[1px] border-[#D1D5DB] font-[Inter] text-[16px] mb-[10px]"
+          onChange={(e) => {
+            handleInputSettingsData("phSms", e.target.value);
+          }}
+        />
+        <Input
+          isRequired
+          value={settings.phEmail}
+          label="Name of Email address field on your Posthog person"
+          placeholder={"$email"}
+          name="name"
+          id="name"
+          className="w-full p-[15px_16px_15px_16px] bg-white border-[1px] border-[#D1D5DB] font-[Inter] text-[16px] mb-[10px]"
+          onChange={(e) => {
+            handleInputSettingsData("phEmail", e.target.value);
+          }}
+        />
+      </form>
+    ),
+  };
 
   return (
     <>
@@ -153,9 +303,11 @@ export default function OnboardingBeta() {
                     </div>
                   </Transition.Child>
                   <div className="flex flex-shrink-0 items-center px-4">
+                    {/* to do our icon will go here */}
+                    {/* src="https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=300" */}
                     <img
                       className="h-8 w-auto"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=300"
+                      src={LaudspeakerIcon}
                       alt="Easywire logo"
                     />
                   </div>
@@ -216,9 +368,10 @@ export default function OnboardingBeta() {
           {/* Sidebar component, swap this element with another sidebar if you like */}
           <div className="flex flex-grow flex-col overflow-y-auto bg-cyan-700 pt-5 pb-4">
             <div className="flex flex-shrink-0 items-center px-4">
+              {/* src="https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=300" */}
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=cyan&shade=300"
+                src={LaudspeakerWhiteIcon}
                 alt="Easywire logo"
               />
             </div>
@@ -269,7 +422,7 @@ export default function OnboardingBeta() {
         </div>
 
         <div className="flex flex-1 flex-col lg:pl-64">
-          <div className="flex h-16 flex-shrink-0 border-b border-gray-200 bg-white lg:border-none">
+          <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 border-b border-gray-200 bg-white">
             <button
               type="button"
               className="border-r border-gray-200 px-4 text-gray-400 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-500 lg:hidden"
@@ -438,6 +591,13 @@ export default function OnboardingBeta() {
             </div>
             {/*this is where we added*/}
             {/*<div className="py-6 md:justify-between lg:border-t lg:border-gray-200">*/}
+
+            <div className="hidden sm:block" aria-hidden="true">
+              <div className="py-5">
+                <div className="border-t border-gray-200" />
+              </div>
+            </div>
+
             <div className="relative mx-auto max-w-4xl md:px-8 xl:px-0">
               {/*<div className="mt-8">*/}
               <div className="mx-auto max-w-6xl px-4 sm:px-6 lg:px-8"></div>
@@ -917,6 +1077,176 @@ export default function OnboardingBeta() {
                     </form>
                   </div>
                 </div>
+              </div>
+            </div>
+            {/*this is where we added*/}
+            <div className="md:grid md:grid-cols-3 md:gap-6">
+              <div className="md:col-span-1 p-5">
+                <div className="px-4 sm:px-0">
+                  <h3 className="text-lg font-medium leading-6 text-gray-900">
+                    Email
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-600">
+                    This information will be displayed publicly so be careful
+                    what you share.
+                  </p>
+                </div>
+              </div>
+              <div className="mt-5 md:col-span-2 pd-5">
+                <form action="#" method="POST">
+                  <div className="shadow sm:overflow-visible sm:rounded-md">
+                    <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                      <h2>Email configuration</h2>
+                      <Input
+                        name="sendingName"
+                        id="sendingName"
+                        label="Sending name"
+                        value={integrationsData.sendingName}
+                        className="w-full p-[15px_16px_15px_16px] bg-white border-[1px] border-[#D1D5DB] font-[Inter] text-[16px] mb-[10px]"
+                        onChange={handleIntegrationsDataChange}
+                      />
+                      <Input
+                        name="sendingEmail"
+                        id="sendingEmail"
+                        label="Sending email"
+                        value={integrationsData.sendingEmail}
+                        className="w-full p-[15px_16px_15px_16px] bg-white border-[1px] border-[#D1D5DB] font-[Inter] text-[16px] mb-[10px]"
+                        onChange={handleIntegrationsDataChange}
+                      />
+                      <Input
+                        isRequired
+                        value={privateApiKey}
+                        label="Private API Key"
+                        placeholder={"****  "}
+                        name="privateApiKey"
+                        id="privateApiKey"
+                        type="password"
+                        className="w-full p-[15px_16px_15px_16px] bg-white border-[1px] border-[#D1D5DB] font-[Inter] text-[16px] mb-[10px]"
+                        labelClass="!text-[16px]"
+                        onChange={(e) => {
+                          setPrivateApiKey(e.target.value);
+                          handleIntegrationsDataChange(e);
+                        }}
+                        onBlur={callDomains}
+                      />
+                      <Select
+                        id="activeJourney"
+                        value={domainName}
+                        options={domainList.map((item: any) => ({
+                          value: item.name,
+                        }))}
+                        onChange={(value) => {
+                          setDomainName(value);
+                        }}
+                        displayEmpty
+                        renderValue={(val: any) => val}
+                        sx={{
+                          height: "44px",
+                          margin: "20px 0px",
+                        }}
+                        inputProps={{
+                          "& .MuiSelect-select": {
+                            padding: "9px 15px",
+                            border: "1px solid #DEDEDE",
+                            boxShadow: "none",
+                            borderRadius: "3px",
+                          },
+                          sx: {
+                            borderRadius: "6px !important",
+                          },
+                        }}
+                      />
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-3 md:gap-6 border-t-[1px]">
+              <div className="md:col-span-1 p-5">
+                <div className="px-4 sm:px-0">
+                  <h3 className="text-lg font-medium leading-6 text-gray-900">
+                    Slack
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-600">
+                    This information will be displayed publicly so be careful
+                    what you share.
+                  </p>
+                </div>
+              </div>
+              <div className="mt-5 md:col-span-2 pd-5">
+                <form action="#" method="POST">
+                  <div className="shadow sm:overflow-hidden sm:rounded-md">
+                    <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                      <h2>Slack configuration</h2>
+                      <a
+                        href={slackInstallUrl}
+                        target="_blank"
+                        rel="noreferrer noopener"
+                      >
+                        <img
+                          alt="add to slack"
+                          src="https://platform.slack-edge.com/img/add_to_slack.png"
+                          srcSet="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+                          width="139"
+                          height="40"
+                        />
+                      </a>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="md:grid md:grid-cols-3 md:gap-6 border-t-[1px]">
+              <div className="md:col-span-1 p-5">
+                <div className="px-4 sm:px-0">
+                  <h3 className="text-lg font-medium leading-6 text-gray-900">
+                    Events
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-600">
+                    This information will be displayed publicly so be careful
+                    what you share.
+                  </p>
+                </div>
+              </div>
+              <div className="mt-5 md:col-span-2 pd-5">
+                <form action="#" method="POST">
+                  <div className="shadow sm:rounded-md">
+                    <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
+                      <h2>Events configuration</h2>
+                      <Select
+                        id="events_config_select"
+                        options={allChannels.map((item: any) => ({
+                          value: item.id,
+                          title: item.title,
+                          disabled: item.disabled,
+                        }))}
+                        value={integrationsData.eventProvider}
+                        onChange={(value: string) =>
+                          setIntegrationsData({
+                            ...integrationsData,
+                            eventProvider: value,
+                          })
+                        }
+                      />
+                      {integrationsData.eventProvider && (
+                        <>
+                          <h3 className="flex items-center text-[18px] font-semibold leading-[40px] mb-[10px]">
+                            {integrationsData.eventProvider
+                              .charAt(0)
+                              .toUpperCase() +
+                              integrationsData.eventProvider.slice(1)}{" "}
+                            Configuration
+                          </h3>
+                          {
+                            parametersToConfigure[
+                              integrationsData.eventProvider
+                            ]
+                          }
+                        </>
+                      )}
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
             {/*this is where we added*/}
