f9b205da3270a2a975b428e467387a84dd46b756
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2022-10-28 12:30:50 +0300

    Feature: onboarding test improvements

--- packages/client/cypress/e2e/02-onboarding.spec.cy.ts
+++ packages/client/cypress/e2e/02-onboarding.spec.cy.ts
@@ -1,6 +1,14 @@
+/* eslint-disable jest/expect-expect */
 import credentials from "../fixtures/credentials.json";
 
-const { email, password, APIkey } = credentials;
+const { email, password } = credentials;
+
+const {
+  TESTS_POSTHOG_API_KEY,
+  TESTS_POSTHOG_HOST_URL,
+  TESTS_POSTHOG_PROJECT_ID,
+  TESTS_MAILGUN_API_KEY,
+} = Cypress.env();
 
 describe("onboarding", () => {
   it("passes", () => {
@@ -9,8 +17,8 @@ describe("onboarding", () => {
     cy.clearCookies();
     cy.clearCookies();
     cy.url().should("include", "/login");
-    cy.get("#email").type(email);
-    cy.get("#password").type(password);
+    cy.get("#email").clear().type(email);
+    cy.get("#password").clear().type(password);
     cy.get("#loginIntoAccount").click();
     cy.contains("Active Journeys").should("exist");
 
@@ -19,9 +27,7 @@ describe("onboarding", () => {
     cy.get('[data-option="mailgun"]').click();
     cy.get("#sendingName").clear().type("SendingName");
     cy.get("#sendingEmail").clear().type("SendingEmail");
-    cy.get("#privateApiKey")
-      .clear()
-      .type("e52ef0112c0c7394b273ba3d3e25474c-4dd50799-4a315eeb");
+    cy.get("#privateApiKey").clear().type(TESTS_MAILGUN_API_KEY);
     cy.get("#activeJourney").click();
     cy.get('[data-option^="sandbox"]').click();
     cy.get("#saveEmailConfiguration").click();
@@ -29,14 +35,27 @@ describe("onboarding", () => {
     cy.get('[href^="https://slack.com/oauth"]').should("exist");
 
     // add mock endpoint for this one
-    // cy.get("#events_config_select").click();
-    // cy.get('[data-option="posthog"]').click();
-    // cy.get("#posthogApiKey").type(
-    //   "test-value-key-test-value-key-test-value-key"
-    // );
-    // cy.get("#posthogProjectId").type("2707");
-    // cy.get("#posthogHostUrl").type("app.posthog.com");
-    // cy.get("#posthogSmsKey").type("phoneNumber");
-    // cy.get("#posthogEmailKey").type("email");
+    cy.get("#events_config_select").click();
+    cy.get('[data-option="posthog"]').click();
+    cy.get("#posthogApiKey").clear().type(TESTS_POSTHOG_API_KEY);
+    cy.get("#posthogProjectId").clear().type(TESTS_POSTHOG_PROJECT_ID);
+    cy.get("#posthogHostUrl").clear().type(TESTS_POSTHOG_HOST_URL);
+    cy.get("#posthogSmsKey").clear().type("phoneNumber");
+    cy.get("#posthogEmailKey").clear().type("email");
+    cy.get(".mt-5 > .bg-gray-50 > .inline-flex").click();
+
+    cy.reload();
+    cy.wait(1000);
+
+    cy.get("#sendingName").should("contain.value", "SendingName");
+    cy.get("#sendingEmail").should("contain.value", "SendingEmail");
+    cy.get("#privateApiKey").should("contain.value", TESTS_MAILGUN_API_KEY);
+    cy.get("#posthogApiKey").should("contain.value", TESTS_POSTHOG_API_KEY);
+    cy.get("#posthogProjectId").should(
+      "contain.value",
+      TESTS_POSTHOG_PROJECT_ID
+    );
+    cy.get("#posthogHostUrl").should("contain.value", TESTS_POSTHOG_HOST_URL);
+    cy.get("#posthogEmailKey").should("contain.value", "email");
   });
 });
