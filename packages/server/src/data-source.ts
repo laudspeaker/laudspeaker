3daa5761618fa3e45bb9e7b0c0e40187e943c8b2
Mahamad Charawi <github@charawi.com>
2022-11-01 16:29:18 -0700

    fixing migrations

--- packages/server/src/data-source.ts
+++ packages/server/src/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
   host: process.env.DATABASE_HOST || 'localhost',
   port: +process.env.DATABASE_PORT || 5432,
   username: process.env.DATABASE_USER || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'postgres',
+  password: process.env.DATABASE_PASSWORD || 'password',
   database: process.env.DATABASE_NAME || 'laudspeaker',
   synchronize: false,
   logging: false,
