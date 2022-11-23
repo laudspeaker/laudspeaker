021950270bd0a43c50912206eac31ed830516a45
Mahamad Charawi <github@charawi.com>
2022-11-23 09:46:05 -0800

    adding linting and new tooltip for dynamic audiences

--- packages/server/src/migrations/1668680654845-SendridApiKey.ts
+++ packages/server/src/migrations/1668680654845-SendridApiKey.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class SendridApiKey1668680654845 implements MigrationInterface {
-    name = 'SendridApiKey1668680654845'
+  name = 'SendridApiKey1668680654845';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "account" ADD "sendgridApiKey" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "account" ADD "sendgridApiKey" character varying`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "account" DROP COLUMN "sendgridApiKey"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "account" DROP COLUMN "sendgridApiKey"`
+    );
+  }
 }
