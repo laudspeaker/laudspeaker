223e94d62bf8bfa763472144c8bd296f602c3490
Mahamad Charawi <github@charawi.com>
2022-11-28 14:34:44 -0800

    added tracing

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,3 +1,5 @@
+// DO NOT IMPORT ANYTHING BEFORE TRACER
+import './tracer';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
