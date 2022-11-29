65151773ab806e3b6ce033507ec7fb2d9c1d5749
Mahamad Charawi <github@charawi.com>
2022-11-29 10:27:52 -0800

    adding ip resolution for datadog

--- packages/server/src/tracer.ts
+++ packages/server/src/tracer.ts
@@ -1,10 +1,12 @@
 import tracer from 'dd-trace';
 import p from '../package.json';
+import fetch from 'node-fetch';
 
 tracer.init({
   version: p.version,
   env: process.env.ENVIRONMENT,
   service: p.name,
+  hostname: fetch('http://169.254.169.254/latest/meta-data/local-ipv4'),
 });
 
 export default tracer;
