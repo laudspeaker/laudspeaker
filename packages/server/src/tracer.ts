bbf1bf0a0f3d78a9b950e537ffd8de78a66a94f3
Mahamad Charawi <github@charawi.com>
2022-11-29 12:10:30 -0800

    adding profiling

--- packages/server/src/tracer.ts
+++ packages/server/src/tracer.ts
@@ -4,6 +4,7 @@ import fetch from 'sync-fetch';
 
 tracer.init({
   version: p.version,
+  profiling: true,
   env: process.env.ENVIRONMENT,
   service: p.name,
   hostname: fetch('http://169.254.169.254/latest/meta-data/local-ipv4'),
