71f5ec018f97d7d1b3ddd632417a0e256f7c0356
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2022-12-15 13:41:39 +0200

    Fix: linting

--- packages/server/src/migrations/1671098271359-SegmentResources.ts
+++ packages/server/src/migrations/1671098271359-SegmentResources.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class SegmentResources1671098271359 implements MigrationInterface {
-    name = 'SegmentResources1671098271359'
+  name = 'SegmentResources1671098271359';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "segment" ADD "resources" jsonb NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "segment" ADD "resources" jsonb NOT NULL`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "segment" DROP COLUMN "resources"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "segment" DROP COLUMN "resources"`);
+  }
 }
