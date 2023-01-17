309ad8a36fd150045bc5e1ac7940903928d21adc
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-01-17 19:00:43 +0200

    Fix: adaptive stepper

--- packages/client/src/components/Elements/Stepper/Stepper.tsx
+++ packages/client/src/components/Elements/Stepper/Stepper.tsx
@@ -9,17 +9,17 @@ export interface StepperProps {
 
 const Stepper: FC<StepperProps> = ({ activeStep, onChange, steps }) => {
   return (
-    <div className="justify-start items-center hidden lg:flex">
+    <div className="flex flex-col lg:flex-row justify-start items-center">
       {steps.map((step, index) => (
         <div
-          className={`relative ${
+          className={`relative w-full ${
             index === activeStep
               ? "border-cyan-500 text-cyan-600"
               : "text-gray-500 hover:text-gray-700"
           }`}
         >
           <div
-            className={`h-[30px] px-6 flex justify-between items-center gap-[5px] cursor-pointer first:rounded-sm`}
+            className={`h-[30px] px-6 flex justify-start lg:justify-between items-center gap-[5px] cursor-pointer first:rounded-sm`}
             onClick={() => onChange && onChange(index)}
           >
             {index < activeStep ? (
@@ -36,7 +36,7 @@ const Stepper: FC<StepperProps> = ({ activeStep, onChange, steps }) => {
             <span>{step}</span>
           </div>
           <div
-            className={`absolute w-full ${
+            className={`absolute w-full hidden lg:block ${
               index === activeStep
                 ? "bg-cyan-500 h-[3px]"
                 : "bg-gray-400 h-[2px]"
