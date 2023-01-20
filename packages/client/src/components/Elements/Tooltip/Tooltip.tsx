aff0bff4fbd9a26623dcdbc69e284bff720f133f
mattyait <mattyait@gmail.com>
2023-01-20 12:22:37 +1100

    testing debug

--- packages/client/src/components/Elements/Tooltip/Tooltip.tsx
+++ packages/client/src/components/Elements/Tooltip/Tooltip.tsx
@@ -1,28 +1,28 @@
-// import React, { FC, ReactNode } from "react";
-// import { Tooltip as MTTooltip, TooltipProps } from "@material-tailwind/react";
+import React, { FC, ReactNode } from "react";
+import { Tooltip as MTTooltip, TooltipProps } from "@material-tailwind/react";
 
-// interface CustomTooltipProps {
-//   children: ReactNode;
-//   title: string;
-//   placement?: string;
-//   className?: string;
-// }
+interface CustomTooltipProps {
+  children: ReactNode;
+  title: string;
+  placement?: string;
+  // className?: string;
+}
 
-// const Tooltip: FC<CustomTooltipProps & TooltipProps> = ({
-//   children,
-//   title,
-//   placement = "top",
-//   className,
-// }) => {
-//   return (
-//     <MTTooltip
-//       className={`${className} text-center z-[10]`}
-//       content={title}
-//       placement={placement}
-//     >
-//       <div>{children}</div>
-//     </MTTooltip>
-//   );
-// };
+const Tooltip: FC<CustomTooltipProps & TooltipProps> = ({
+  children,
+  title,
+  placement = "top",
+  // className,
+}) => {
+  return (
+    <MTTooltip
+      // className={`${className} text-center z-[10]`}
+      content={title}
+      placement={placement}
+    >
+      <div>{children}</div>
+    </MTTooltip>
+  );
+};
 
-// export default Tooltip;
+export default Tooltip;
