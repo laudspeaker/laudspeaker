dec07160dc8442bec75368166dc98175d6060297
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-01-25 21:02:03 +0200

    Feature: four step with all time triggers test

--- packages/client/cypress/test-helpers/setupSpecificTimeTrigger.ts
+++ packages/client/cypress/test-helpers/setupSpecificTimeTrigger.ts
@@ -1,12 +1,12 @@
 import setDate from "./setDate";
 import { format } from "date-fns";
 
-export default () => {
+export default (offset = 60000) => {
   cy.get("#timeDelay > .p-0 > .justify-between").click();
   cy.get(".MuiFormControl-root button").click();
   cy.get("#data-option-SpecificTime").click();
   cy.get(".css-1lwbda4-MuiStack-root > input").then((input) => {
-    const date = new Date(new Date().getTime() + 60000);
+    const date = new Date(new Date().getTime() + offset);
 
     setDate(input[0], format(date, "yyyy-MM-dd") + "T" + format(date, "hh:mm"));
   });
