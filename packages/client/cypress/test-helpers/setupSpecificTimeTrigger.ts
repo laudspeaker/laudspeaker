fbff15067137d41593d802e166c4ad305729c71d
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-01-25 11:28:31 +0200

    Feature: specific time trigger tests

---
+++ packages/client/cypress/test-helpers/setupSpecificTimeTrigger.ts
@@ -0,0 +1,15 @@
+import setDate from "./setDate";
+import { format } from "date-fns";
+
+export default () => {
+  cy.get("#timeDelay > .p-0 > .justify-between").click();
+  cy.get(".MuiFormControl-root button").click();
+  cy.get("#data-option-SpecificTime").click();
+  cy.get(".css-1lwbda4-MuiStack-root > input").then((input) => {
+    const date = new Date(new Date().getTime() + 60000);
+
+    setDate(input[0], format(date, "yyyy-MM-dd") + "T" + format(date, "hh:mm"));
+  });
+  cy.wait(1000);
+  cy.get("#save-trigger").click();
+};
