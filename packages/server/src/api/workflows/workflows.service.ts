e612140abb1b92ff57902d58ea738e9a4cee8659
Mahamad Charawi <github@charawi.com>
2023-01-26 18:40:10 -0800

    Converting account id to string.

--- packages/server/src/api/workflows/workflows.service.ts
+++ packages/server/src/api/workflows/workflows.service.ts
@@ -446,7 +446,7 @@ export class WorkflowsService {
     try {
       this.logger.debug(`workflow.service.ts:WorkflowService.start: Account attempting to start workflow: ${JSON.stringify(account, null, 2)}`);
       const job = await this.eventsQueue.add('start', {
-        accountId: account.id,
+        accountId: account.id.toString(),
         workflowID,
       });
       const data = await job.finished();
