30c290864f6ecac668d590b11336ae4a888d31b5
Mahamad Charawi <github@charawi.com>
2023-02-01 11:45:19 -0800

    Adding sms processor to module.

--- packages/server/src/api/sms/sms.module.ts
+++ packages/server/src/api/sms/sms.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
 import { SmsController } from './sms.controller';
 import { SmsProcessor } from './sms.processor';
 import { SmsService } from './sms.service';
 
 @Module({
+  imports: [
+    BullModule.registerQueue({
+      name: 'sms',
+    }),
+  ],
   controllers: [SmsController],
   providers: [SmsService, SmsProcessor],
   exports: [SmsService],
 })
-export class SmsModule {}
+export class SmsModule { }
