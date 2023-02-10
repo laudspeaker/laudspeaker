0f77a3e9a104d081300442bd2976647001860cda
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-02-10 19:29:31 +0200

    Fix: onboarding tabs flow

--- packages/client/src/pages/Onboarding/OnboardingBeta.tsx
+++ packages/client/src/pages/Onboarding/OnboardingBeta.tsx
@@ -1632,10 +1632,21 @@ export default function OnboardingBeta() {
                   name="selected-tab"
                   className="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-cyan-500 focus:outline-none focus:ring-cyan-500 sm:text-sm"
                   value={currentStep}
-                  onChange={(e) => setCurrentStep(+e.currentTarget.value)}
+                  onChange={(e) => {
+                    if (+e.currentTarget.value > currentStep)
+                      setIsNextItemAvailable(false);
+                    setCurrentStep(+e.currentTarget.value);
+                  }}
                 >
                   {[0, 1, 2].map((tab) => (
-                    <option key={tab} value={tab}>
+                    <option
+                      key={tab}
+                      value={tab}
+                      disabled={
+                        tab > currentStep &&
+                        !(tab === currentStep + 1 && isNextItemAvailable)
+                      }
+                    >
                       {tabNames[tab]}
                     </option>
                   ))}
@@ -1651,9 +1662,24 @@ export default function OnboardingBeta() {
                           tab === currentStep
                             ? "border-cyan-500 text-cyan-600"
                             : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
-                          "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer"
+                          "whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm cursor-pointer",
+                          `${
+                            tab > currentStep &&
+                            !(tab === currentStep + 1 && isNextItemAvailable)
+                              ? "grayscale"
+                              : ""
+                          }`
                         )}
-                        onClick={() => setCurrentStep(tab)}
+                        onClick={
+                          tab > currentStep &&
+                          !(tab === currentStep + 1 && isNextItemAvailable)
+                            ? undefined
+                            : () => {
+                                if (tab > currentStep)
+                                  setIsNextItemAvailable(false);
+                                setCurrentStep(tab);
+                              }
+                        }
                       >
                         {tabNames[tab]}
                       </div>
