748a54cf2facfa3119d1333ca00dba9d0486d5e6
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-02-21 17:41:01 +0200

    Fix: test 25

--- packages/client/cypress/test-helpers/runTwoStepEmailJourney.ts
+++ packages/client/cypress/test-helpers/runTwoStepEmailJourney.ts
@@ -13,13 +13,15 @@ export default (name: string, eventName: string, flowName = "Email flow") => {
   cy.get("#audience > .p-0 > .justify-between").click();
   cy.get("#name").type("Initial");
   cy.contains("Finish later").click();
-  cy.get(".text-updater").move({ deltaX: 100, deltaY: 100 });
+  cy.get(".text-updater").move({ deltaX: 100, deltaY: 100, force: true });
+  cy.wait(3000);
   cy.get("#audience > .p-0 > .justify-between").click();
   cy.get("#name").type("Second");
   cy.contains("Finish later").click();
   cy.get('[data-isprimary]:not([data-isprimary="true"])').move({
     deltaX: 100,
     deltaY: 300,
+    force: true,
   });
   cy.get('[data-isprimary]:not([data-isprimary="true"])').click();
   cy.get("#email > .p-0 > .justify-between").click();
