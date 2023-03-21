89f432afdb10fb8822928e58cb416b74ff960d99
Mahamad Charawi <github@charawi.com>
2023-03-21 15:48:21 -0700

    Adding Render Dockerfile and running migrations.

--- packages/server/src/shared/typeorm/typeorm.service.ts
+++ packages/server/src/shared/typeorm/typeorm.service.ts
@@ -1,13 +1,11 @@
-import { Injectable } from '@nestjs/common';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-@Injectable()
 export class TypeOrmConfigService implements TypeOrmOptionsFactory {
   public createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
       type: 'postgres',
       host: process.env.DATABASE_HOST || 'localhost',
-      port: +process.env.DATABASE_PORT || 5432,
+      port: process?.env?.DATABASE_PORT ? +process.env.DATABASE_PORT : 5432,
       database: process.env.DATABASE_NAME || 'laudspeaker',
       username: process.env.DATABASE_USER || 'postgres',
       password: process.env.DATABASE_PASSWORD || 'Qcezadwx311',
