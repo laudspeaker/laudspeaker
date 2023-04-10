81f674aa9cc5b503788f795e2051773dde8b1349
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-04-10 18:08:10 +0300

    Fix: uncomment tests

--- packages/client/cypress/e2e/69-api-calls.spec.cy.ts
+++ packages/client/cypress/e2e/69-api-calls.spec.cy.ts
@@ -23,80 +23,80 @@ describe("Api calls", { env: { AxiosURL: "http://localhost:3001/" } }, () => {
     cy.wait(1000);
   });
 
-  // it("passes for email", () => {
-  //   loginFunc(email, password);
+  it("passes for email", () => {
+    loginFunc(email, password);
 
-  //   cy.contains("Messaging").click();
-  //   cy.get('[data-disclosure-link="Template Builder"]').click();
-  //   cy.get("#createTemplate").click();
-  //   cy.get("#name").clear().type("ApiCallsEmail");
-  //   cy.get("#handleTemplateType").click();
-  //   cy.get("#handleTemplateType").find('[data-option="email"]').click();
-  //   cy.get("#submitTemplateCreation").click();
-  //   cy.get('[data-custominput-placeholder="Subject"]').click({
-  //     force: true,
-  //   });
-  //   cy.wait(1000);
-  //   cy.get(":nth-child(1) > :nth-child(2) > .relative > #title")
-  //     .clear()
-  //     .type(apiCallTagToInsert, { parseSpecialCharSequences: false });
-  //   cy.get("#saveDraftTemplate").click();
-  //   cy.wait(1000);
-  //   cy.reload();
-  //   cy.contains("response.data.id").click();
-  //   cy.contains("Host: jsonplaceholder.typicode.com").should("exist");
-  //   cy.get(".h-8").click({ force: true });
+    cy.contains("Messaging").click();
+    cy.get('[data-disclosure-link="Template Builder"]').click();
+    cy.get("#createTemplate").click();
+    cy.get("#name").clear().type("ApiCallsEmail");
+    cy.get("#handleTemplateType").click();
+    cy.get("#handleTemplateType").find('[data-option="email"]').click();
+    cy.get("#submitTemplateCreation").click();
+    cy.get('[data-custominput-placeholder="Subject"]').click({
+      force: true,
+    });
+    cy.wait(1000);
+    cy.get(":nth-child(1) > :nth-child(2) > .relative > #title")
+      .clear()
+      .type(apiCallTagToInsert, { parseSpecialCharSequences: false });
+    cy.get("#saveDraftTemplate").click();
+    cy.wait(1000);
+    cy.reload();
+    cy.contains("response.data.id").click();
+    cy.contains("Host: jsonplaceholder.typicode.com").should("exist");
+    cy.get(".h-8").click({ force: true });
 
-  //   runEmailJourney("Api calls email test", "ApiCallsEmail");
-  //   cy.wait(5000);
-  //   cy.reload();
-  //   cy.get(".justify-between > :nth-child(1) > .font-medium").then((el) => {
-  //     expect(Number(el.text())).to.be.greaterThan(0);
-  //   });
-  // });
+    runEmailJourney("Api calls email test", "ApiCallsEmail");
+    cy.wait(5000);
+    cy.reload();
+    cy.get(".justify-between > :nth-child(1) > .font-medium").then((el) => {
+      expect(Number(el.text())).to.be.greaterThan(0);
+    });
+  });
 
-  // it("passes for slack", () => {
-  //   loginFunc(email, password);
+  it("passes for slack", () => {
+    loginFunc(email, password);
 
-  //   cy.contains("Messaging").click();
-  //   cy.get('[data-disclosure-link="Template Builder"]').click();
-  //   cy.url().should("include", "/templates");
-  //   cy.get("#createTemplate").click();
-  //   cy.get("#name").clear().type("ApiCallsSlack");
-  //   cy.get("#handleTemplateType").click();
-  //   cy.get("#handleTemplateType").find('[data-option="slack"]').click();
-  //   cy.get("#submitTemplateCreation").click();
-  //   cy.get('[data-custominput-placeholder="Slack Message"]').click("left");
-  //   cy.get(".relative > #slackMessage")
-  //     .clear({ force: true })
-  //     .type(apiCallTagToInsert, { parseSpecialCharSequences: false });
-  //   cy.get("#saveDraftTemplate").click();
-  //   runSlackJourney("Api calls slack test", "ApiCallsSlack");
-  // });
+    cy.contains("Messaging").click();
+    cy.get('[data-disclosure-link="Template Builder"]').click();
+    cy.url().should("include", "/templates");
+    cy.get("#createTemplate").click();
+    cy.get("#name").clear().type("ApiCallsSlack");
+    cy.get("#handleTemplateType").click();
+    cy.get("#handleTemplateType").find('[data-option="slack"]').click();
+    cy.get("#submitTemplateCreation").click();
+    cy.get('[data-custominput-placeholder="Slack Message"]').click("left");
+    cy.get(".relative > #slackMessage")
+      .clear({ force: true })
+      .type(apiCallTagToInsert, { parseSpecialCharSequences: false });
+    cy.get("#saveDraftTemplate").click();
+    runSlackJourney("Api calls slack test", "ApiCallsSlack");
+  });
 
-  // it("passes for sms", () => {
-  //   loginFunc(email, password);
+  it("passes for sms", () => {
+    loginFunc(email, password);
 
-  //   cy.contains("Messaging").click();
-  //   cy.get('[data-disclosure-link="Template Builder"]').click();
-  //   cy.url().should("include", "/templates");
-  //   cy.get("#createTemplate").click();
-  //   cy.get("#name").clear().type("ApiCallsSms");
-  //   cy.get("#handleTemplateType").click();
-  //   cy.get("#handleTemplateType").find('[data-option="sms"]').click();
-  //   cy.get("#submitTemplateCreation").click();
-  //   cy.get('[data-custominput-placeholder="SMS Message"]').click("left");
-  //   cy.get(".relative > #smsMessage")
-  //     .clear()
-  //     .type(apiCallTagToInsert, { parseSpecialCharSequences: false });
-  //   cy.get("#saveDraftTemplate").click();
-  //   runSmsJourney("Api calls slack test", "ApiCallsSms");
-  //   cy.wait(5000);
-  //   cy.reload();
-  //   cy.get(".justify-between > :nth-child(1) > .font-medium").then((el) => {
-  //     expect(Number(el.text())).to.be.greaterThan(0);
-  //   });
-  // });
+    cy.contains("Messaging").click();
+    cy.get('[data-disclosure-link="Template Builder"]').click();
+    cy.url().should("include", "/templates");
+    cy.get("#createTemplate").click();
+    cy.get("#name").clear().type("ApiCallsSms");
+    cy.get("#handleTemplateType").click();
+    cy.get("#handleTemplateType").find('[data-option="sms"]').click();
+    cy.get("#submitTemplateCreation").click();
+    cy.get('[data-custominput-placeholder="SMS Message"]').click("left");
+    cy.get(".relative > #smsMessage")
+      .clear()
+      .type(apiCallTagToInsert, { parseSpecialCharSequences: false });
+    cy.get("#saveDraftTemplate").click();
+    runSmsJourney("Api calls slack test", "ApiCallsSms");
+    cy.wait(5000);
+    cy.reload();
+    cy.get(".justify-between > :nth-child(1) > .font-medium").then((el) => {
+      expect(Number(el.text())).to.be.greaterThan(0);
+    });
+  });
 
   it("passes for firebase", () => {
     loginFunc(email, password);
