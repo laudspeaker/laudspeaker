99cf2ec81bfce0fcfcb381b68b4c1f6921be6179
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-04-10 14:01:56 +0300

    Fix: webhook tests

--- packages/client/cypress/e2e/67-two-step-webhook-POST-with-merge.spec.cy.ts
+++ packages/client/cypress/e2e/67-two-step-webhook-POST-with-merge.spec.cy.ts
@@ -25,43 +25,25 @@ describe(
       startWebhookJouney("webhook2");
 
       cy.wait(5000);
-      cy.reload();
-      cy.get(".justify-between > :nth-child(1) > .font-medium").then((el) => {
-        expect(Number(el.text())).greaterThan(0);
+
+      cy.request({
+        method: "POST",
+        url: `${Cypress.env("AxiosURL")}events`,
+        headers: {
+          Authorization: `Api-Key ${userAPIkey}`,
+        },
+        body: {
+          correlationKey: "email",
+          correlationValue: emailTemplate.correlationValue,
+          event: { wh: "wh" },
+        },
+      }).then(() => {
+        cy.wait(5000);
+        cy.reload();
+        cy.get(".justify-between > :nth-child(1) > .font-medium").then((el) => {
+          expect(Number(el.text())).greaterThan(0);
+        });
       });
-
-      // cy.request({
-      //   method: "POST",
-      //   url: `${Cypress.env("AxiosURL")}events`,
-      //   headers: {
-      //     Authorization: `Api-Key ${userAPIkey}`,
-      //   },
-      //   body: {
-      //     correlationKey: "email",
-      //     correlationValue: emailTemplate.correlationValue,
-      //     event: { wh: "wh" },
-      //   },
-      // }).then(({ body }) => {
-      //   const webhookBody = JSON.parse(body[0]?.allJobData?.[0]?.body);
-      //   expect(webhookBody?.userId).to.eq("testmail@gmail.com");
-      //   expect(webhookBody?.templatesExample).to.eq(
-      //     "How was your recent experience?"
-      //   );
-
-      //   cy.wait(5000);
-      //   cy.request({
-      //     method: "POST",
-      //     headers: {
-      //       Authorization: `Api-Key ${userAPIkey}`,
-      //     },
-      //     url: `${Cypress.env("AxiosURL")}events/job-status/webhook`,
-      //     body: {
-      //       jobId: body[0]?.jobIds?.[0],
-      //     },
-      //   }).then(({ body }) => {
-      //     expect(body).to.equal("completed");
-      //   });
-      // });
     });
   }
 );
