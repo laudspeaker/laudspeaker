d7708baba5ed30564ef758d07de0dfadab42b2e3
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-04-27 14:51:17 +0300

    Fix: modal preview

--- packages/client/src/pages/ModalBuilder/ModalPreview.tsx
+++ packages/client/src/pages/ModalBuilder/ModalPreview.tsx
@@ -1,15 +1,12 @@
-import { FC, useRef } from "react";
+import React, { FC } from "react";
 import { ModalState } from "./ModalBuilder";
-import laudspeakerjs from "@laudspeaker/laudspeaker-js";
-import { createRoot } from "react-dom/client";
+import { Modal } from "@laudspeaker/laudspeaker-js";
+
 interface ModalPreviewProps {
   modalState: ModalState;
 }
+
 const ModalPreview: FC<ModalPreviewProps> = ({ modalState }) => {
-  const rootRef = useRef<HTMLDivElement>(null);
-  if (rootRef.current) {
-    laudspeakerjs.renderPreview(createRoot(rootRef.current), modalState);
-  }
-  return <div ref={rootRef}></div>;
+  return <Modal modalState={modalState} />;
 };
 export default ModalPreview;
