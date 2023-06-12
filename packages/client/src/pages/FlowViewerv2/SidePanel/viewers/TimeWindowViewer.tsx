dafbad2440fc45b6863f4455995a09d3c745cc22
mykolalaudspeaker31 <mykola.laudspeaker31@gmail.com>
2023-06-12 17:00:45 +0300

    feature: added style

--- packages/client/src/pages/FlowViewerv2/SidePanel/viewers/TimeWindowViewer.tsx
+++ packages/client/src/pages/FlowViewerv2/SidePanel/viewers/TimeWindowViewer.tsx
@@ -1,11 +1,25 @@
 import { TimeWindowNodeData } from "pages/FlowBuilderv2/Nodes/NodeData";
 import React, { FC } from "react";
 import { SidePanelComponentProps } from "../FlowViewerSidePanel";
+import { format } from "date-fns";
 
-const TimeWindowViewer: FC<
-  SidePanelComponentProps<TimeWindowNodeData>
-> = () => {
-  return <div>TimeWindowViewer</div>;
+const TimeWindowViewer: FC<SidePanelComponentProps<TimeWindowNodeData>> = ({
+  nodeData,
+}) => {
+  const from = format(new Date(nodeData.from), "yyyy/MM/dd kk:mm");
+  const to = format(new Date(nodeData.to), "yyyy/MM/dd kk:mm");
+  return (
+    <div className="bg-gray-100 border border-gray-200 rounded gap-5 flex px-2 py-3">
+      <div className="flex flex-col">
+        <span>From</span>
+        <span>To</span>
+      </div>
+      <div className="flex flex-col font-semibold">
+        <span>{from}</span>
+        <span>{to}</span>
+      </div>
+    </div>
+  );
 };
 
 export default TimeWindowViewer;
