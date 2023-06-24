4c254fb0b9025ac654823655f3960a33d88ccbc6
Abheek Basu <abheek94@gmail.com>
2023-06-24 13:52:57 -0400

    button colour changes

--- packages/client/src/components/Elements/Button/GenericButton/GenericButton.tsx
+++ packages/client/src/components/Elements/Button/GenericButton/GenericButton.tsx
@@ -32,7 +32,7 @@ const GenericButton = (props: ButtonProps) => {
     <button
       id={id}
       type="button"
-      className={`transition-[0.3s] relative inline-flex items-center rounded-md border-0 border-transparent text-white bg-cyan-600 px-6 py-3 text-base font-medium shadow-sm hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 ${
+      className={`transition-[0.3s] relative inline-flex items-center inline-flex items-center border border-transparent bg-[#6366F1] px-6 py-3 text-base font-medium text-white shadow-sm hover:border-[#818CF8] hover:bg-[#818CF8] focus:border-[#4338CA] focus:bg-[#4338CA] disabled:!bg-[#F3F4F6] rounded-md ${
         disabled || loading ? "grayscale" : ""
       } ${customClasses ? customClasses : ""} `}
       onClick={(ev) => {
