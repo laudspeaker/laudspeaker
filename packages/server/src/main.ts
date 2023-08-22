b955eba5c464023b9ed357d1a12066302d857116
Mahamad Charawi <mahamad@laudspeaker.com>
2023-08-22 15:40:59 -0700

    More cors...

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -34,13 +34,14 @@ async function bootstrap() {
   app.use(urlencoded({ verify: rawBodyBuffer, extended: true }));
   if (process.env.SERVE_CLIENT_FROM_NEST) app.setGlobalPrefix('api');
   app.set('trust proxy', 1);
-  app.enableCors({
-    credentials: true,
-    origin: function (origin, callback) {
-      console.log(`Origin ${origin} is being granted CORS access`);
-      callback(null, true)
-    }
-  });
+  const options = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true
+  };
+  app.enableCors(options);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   const morganMiddleware = morgan(
