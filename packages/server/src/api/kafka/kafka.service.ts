fd15fe6f6a1897b9b81593696a4a9ea1e7d210d0
Jared Hanson <jred0011@gmail.com>
2023-12-05 12:18:39 -0800

    Final sasl configured successfully

--- packages/server/src/api/kafka/kafka.service.ts
+++ packages/server/src/api/kafka/kafka.service.ts
@@ -10,11 +10,25 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 @Injectable()
 export class KafkaService implements OnModuleDestroy, OnModuleInit {
-  private KAFKA_BROKERS = process.env.KAFKA_BROKERS.split(',');
+  private KAFKA_BROKERS = process.env.KAFKA_BROKERS.split(','); // Pass brokers as comma separated list
+  private KAFKA_SASL_USERNAME = process.env.KAFKA_SASL_USERNAME; // Confluent cluster API key
+  private KAFKA_SASL_PASSWORD = process.env.KAFKA_SASL_PASSWORD; // Confluent cluster API secret
+  private KAFKA_SSL = !!this.KAFKA_SASL_PASSWORD && !!this.KAFKA_SASL_USERNAME; // SSL set when SASL user/pass given
+
   // Create the client with the broker list
   private kafka = new Kafka({
     clientId: 'laudspeaker-server',
     brokers: this.KAFKA_BROKERS,
+    ssl: this.KAFKA_SSL,
+    ...(this.KAFKA_SSL
+      ? {
+          sasl: {
+            mechanism: 'plain',
+            username: this.KAFKA_SASL_USERNAME,
+            password: this.KAFKA_SASL_PASSWORD,
+          },
+        }
+      : undefined),
   });
 
   private CACHE: { producer?: Producer } = {};
