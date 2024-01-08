4ae1e084a2fece5404f9b311a1eede29bc75fedc
mykolalaudspeaker31 <113930749+mykolalaudspeaker31@users.noreply.github.com>
2024-01-08 20:27:10 +0200

    feat: add customer attribute key validation (#239)

---
+++ packages/server/src/utils/customer-key-name-validator.ts
@@ -0,0 +1,32 @@
+import { BadRequestException } from '@nestjs/common';
+
+export const KEYS_TO_SKIP = [
+  '__v',
+  '_id',
+  'id',
+  'ownerId',
+  'workspaceId',
+  'verified',
+  'journeys',
+  'journeyEnrollmentsDates',
+  'verified',
+  'isAnonymous',
+  'isFreezed',
+  'customComponents',
+];
+
+export const validateKeyForMutations = (val: any) => {
+  const strVal = String(val);
+
+  if (
+    !strVal ||
+    KEYS_TO_SKIP.includes(strVal) ||
+    strVal.startsWith('__') ||
+    strVal.startsWith('_') ||
+    strVal.startsWith('$')
+  )
+    throw new BadRequestException(
+      'Inappropriate key name to create attribute. Please try another one.'
+    );
+};
+
