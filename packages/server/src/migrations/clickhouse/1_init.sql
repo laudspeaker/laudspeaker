a8b1ee125606173d5976a111741c3474a0da486f
Mahamad Charawi <mahamad@laudspeaker.com>
2024-01-15 14:43:51 -0800

    undo formatting of clickhouse migration

--- packages/server/src/migrations/clickhouse/1_init.sql
+++ packages/server/src/migrations/clickhouse/1_init.sql
@@ -1,23 +1,8 @@
-CREATE TABLE
-    IF NOT EXISTS message_status (
-        stepId UUID,
-        customerId String,
-        templateId String,
-        messageId String,
-        event String,
-        eventProvider String,
-        createdAt DateTime,
-        processed Boolean,
-        userId UUID
-    ) ENGINE = ReplacingMergeTree PRIMARY KEY (
-        stepId,
-        customerId,
-        templateId,
-        messageId,
-        event,
-        eventProvider,
-        createdAt
-    );
+CREATE TABLE IF NOT EXISTS message_status
+(audienceId UUID, customerId String, templateId String, messageId String, event String, eventProvider String, createdAt DateTime, processed Boolean, userId String) 
+ENGINE = ReplacingMergeTree
+PRIMARY KEY (audienceId, customerId, templateId, messageId, event, eventProvider, createdAt);
+
 
 -- -- READ FIRST --
 -- -- The rest of this migration allows you to ingest
