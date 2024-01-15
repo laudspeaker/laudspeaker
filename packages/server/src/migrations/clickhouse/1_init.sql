d76338fb5efff6a2640902201d952c39ab2a0174
Mahamad Charawi <mahamad@laudspeaker.com>
2024-01-15 14:55:28 -0800

    Reverting formatting.

--- packages/server/src/migrations/clickhouse/1_init.sql
+++ packages/server/src/migrations/clickhouse/1_init.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS message_status
-(audienceId UUID, customerId String, templateId String, messageId String, event String, eventProvider String, createdAt DateTime, processed Boolean, userId String) 
+(stepId UUID, customerId String, templateId String, messageId String, event String, eventProvider String, createdAt DateTime, processed Boolean, userId UUID) 
 ENGINE = ReplacingMergeTree
-PRIMARY KEY (audienceId, customerId, templateId, messageId, event, eventProvider, createdAt);
+PRIMARY KEY (stepId, customerId, templateId, messageId, event, eventProvider, createdAt);
 
 
 -- -- READ FIRST --
@@ -10,6 +10,7 @@ PRIMARY KEY (audienceId, customerId, templateId, messageId, event, eventProvider
 -- -- In a cloud environment, this connection will be setup using the 
 -- -- cloud's GUI or other means. This migration should only work
 -- -- in local development, or similar environments.
+
 -- -- create kafka ingestion
 -- CREATE TABLE IF NOT EXISTS message_status_kafka
 -- (
@@ -33,6 +34,7 @@ PRIMARY KEY (audienceId, customerId, templateId, messageId, event, eventProvider
 --     kafka_format = 'JSONEachRow',
 --     date_time_input_format='best_effort'
 -- ;
+
 -- -- create materialized view to copy kafka queue data into message_status table
 -- CREATE MATERIALIZED VIEW message_status_mv TO message_status AS
 --     SELECT *
