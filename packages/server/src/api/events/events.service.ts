806999441555fe2324d37ea674c1ba9e331a2b17
Abheek Basu <abheek94@gmail.com>
2024-01-18 13:38:06 -0500

    journey push

--- packages/server/src/api/events/events.service.ts
+++ packages/server/src/api/events/events.service.ts
@@ -593,7 +593,6 @@ export class EventsService {
             }
 
             const messaging = admin.messaging(firebaseApp);
-
             await messaging.send({
               token:
                 platform === PushPlatforms.ANDROID
@@ -606,32 +605,28 @@ export class EventsService {
               android:
                 platform === PushPlatforms.ANDROID
                   ? {
-                      notification: {
-                        sound: 'default',
-                        imageUrl: settings?.image?.imageSrc || '',
-                      },
-                    }
+                    notification: {
+                      sound: 'default',
+                      imageUrl: settings?.image?.imageSrc || '',
+                    },
+                  }
                   : undefined,
               apns:
                 platform === PushPlatforms.IOS
                   ? {
-                      payload: {
-                        aps: {
-                          badge: 1,
-                          sound: 'default',
-                          'content-available': 1,
-                          category: settings.clickBehavior?.type,
-                          'mutable-content': 1,
-                        },
+                    payload: {
+                      aps: {
+                        badge: 1,
+                        sound: 'default',
+                        category: settings.clickBehavior?.type,
+                        contentAvailable: true,
+                        mutableContent: true,
                       },
-                      ...(settings?.image?.imageSrc
-                        ? {
-                            fcmOptions: {
-                              imageUrl: settings?.image?.imageSrc,
-                            },
-                          }
-                        : {}),
-                    }
+                    },
+                    fcmOptions: {
+                      imageUrl: settings?.image?.imageSrc,
+                    },
+                  }
                   : undefined,
               data: body.pushObject.fields.reduce((acc, field) => {
                 acc[field.key] = field.value;
