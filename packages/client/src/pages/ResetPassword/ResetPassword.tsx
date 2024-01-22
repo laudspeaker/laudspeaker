eb5521e8d87fd569ca59d867db9788bc821cf4c7
Abheek Basu <abheek94@gmail.com>
2024-01-21 19:26:37 -0500

    fixed reset password

--- packages/client/src/pages/ResetPassword/ResetPassword.tsx
+++ packages/client/src/pages/ResetPassword/ResetPassword.tsx
@@ -121,7 +121,7 @@ const ResetPassword = () => {
             <div>
               <GenericButton
                 id="loginIntoAccount"
-                customClasses="flex w-full justify-center rounded-md border border-transparent bg-cyan-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2"
+                customClasses="flex w-full justify-center rounded-md border border-transparent bg-[#6366F1] py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-[#818CF8] focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2"
                 onClick={id ? handleResetPassword : handleSendLink}
                 loading={isSaving}
               >
