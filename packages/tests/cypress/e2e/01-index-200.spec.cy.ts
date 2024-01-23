579f509e725219cb1a911737e89eab9399c14aae
Jared Hanson <jred0011@gmail.com>
2024-01-22 18:34:25 -0800

    Only commit passing tests

--- packages/tests/cypress/e2e/01-index-200.spec.cy.ts
+++ packages/tests/cypress/e2e/01-index-200.spec.cy.ts
@@ -1,7 +1,4 @@
 describe("Initial testing", () => {
-  it("failes loading index", () => {
-    cy.request("/api/accounts").then((resp) => expect(resp.status).to.eq(200));
-  });
   it("successfully loads index", () => {
     cy.visit("/");
   });
--- packages/tests/cypress/e2e/02-index-500.spec.cy.ts
+++
@@ -1,5 +0,0 @@
-describe("Initial failed testing", () => {
-  it("failes loading index", () => {
-    cy.request("/api/accounts").then((resp) => expect(resp.status).to.eq(200));
-  });
-});
