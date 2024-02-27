2b04e4819de21e06219d9d4d30c369cc7611633a
Abheek Basu <abheek94@gmail.com>
2024-02-26 21:26:41 -0500

    tests passing

--- packages/tests/cypress/e2e/020-audience-segments-create-segment-dynamic-rules.spec.cy.ts
+++ packages/tests/cypress/e2e/020-audience-segments-create-segment-dynamic-rules.spec.cy.ts
@@ -1,16 +1,34 @@
 import credentials from "../fixtures/credentials";
 import { loginFunc } from "../test-helpers/loginFunc";
+import signup from "../test-helpers/signup";
+import { setupOrganization } from "../test-helpers/setupOrganization";
 
-const { email, password, firstName, lastName } = credentials;
+const { email, password, firstName, lastName, organizationName, timeZone } = credentials;
 
 describe("charawi", () => {
+
+    beforeEach(() => {
+      cy.request(`${Cypress.env("TESTS_API_BASE_URL")}/tests/reset-tests`);
+      cy.wait(1000);
+      cy.clearAllCookies();
+      cy.clearAllLocalStorage();
+      cy.clearAllSessionStorage();
+      signup(email, password, firstName, lastName);
+      cy.wait(1000);
+      setupOrganization(organizationName, timeZone);
+      cy.wait(10000);
+      cy.visit("/home");
+      cy.url().should("include", "/home");
+
+    });
+
     it("tests charawi", () => {
       //AND
       //String contains "Charawi" ==> 1 Person
       cy.visit("/");
       cy.wait(2000);
-      loginFunc(email, password);
-      cy.wait(2000);
+      //loginFunc(email, password);
+      //cy.wait(2000);
       cy.visit("/segment");
       cy.get('[id*="headlessui-popover-button-"] > button').click();
       cy.get("div:nth-of-type(2) > div.relative div:nth-of-type(2)").click();
