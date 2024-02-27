8fd5feba132dbdcff0f03908525fd0984d15ca8d
abheek9 <abheek94@gmail.com>
2024-02-27 14:34:08 -0500

    remove component templates (#303)

--- packages/client/src/components/Drawer/Drawer.fixtures.tsx
+++ packages/client/src/components/Drawer/Drawer.fixtures.tsx
@@ -35,13 +35,6 @@ export const dataSubArray = [
         link: "/templates",
       },
       {
-        id: "tracker-template",
-        imgIcon: templateBuilderIcon,
-        text: "Component templates",
-        type: "menu",
-        link: "/tracker-template",
-      },
-      {
         id: "event-tracker",
         imgIcon: templateBuilderIcon,
         text: "Event Tracker",
