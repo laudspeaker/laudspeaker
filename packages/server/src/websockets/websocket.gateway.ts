b4dc36604bab54445f7060a06ea7707baf059124
Abheek Basu <abheek94@gmail.com>
2024-03-05 14:20:41 -0500

    ne customer id

--- packages/server/src/websockets/websocket.gateway.ts
+++ packages/server/src/websockets/websocket.gateway.ts
@@ -962,11 +962,17 @@ export class WebsocketGateway implements OnGatewayConnection {
       await this.customersService.CustomerModel.findOne({
         workspaceId: workspace.id,
         [type === PushPlatforms.ANDROID ? 'androidFCMTokens' : 'iosFCMTokens']:
-          token,
+          token, 
+          _id: {
+            $ne: customerId
+           }
       }).exec();
 
+    //console.log("printing conflicting customer");
+    //console.log(JSON.stringify(conflictingCustomer, null, 2));
+
     if (conflictingCustomer) {
-      if (!customer.isAnonymous)
+      if (!customer.isAnonymous)//&&(!conflictingCustomer.isAnonymous))
         throw new WsException(
           'Non-anonymous conflicting customers with this FCM token found'
         );
