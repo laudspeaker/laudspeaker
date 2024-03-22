078a8938efc392f7d5f24117275d849fe87fe4fc
Jared Hanson <jred0011@gmail.com>
2024-03-22 15:27:21 -0700

    Fix s3 upload issue by adding minio to docker compose

--- packages/server/src/api/s3/s3.service.ts
+++ packages/server/src/api/s3/s3.service.ts
@@ -17,7 +17,12 @@ export class S3Service {
   s3 = new AWS.S3({
     accessKeyId: process.env.AWS_S3_ACCESS_KEY,
     secretAccessKey: process.env.AWS_S3_KEY_SECRET,
-    region: process.env.AWS_S3_BUCKET_REGION,
+    ...(process.env.MINIO_S3_URL
+      ? {
+          endpoint: process.env.MINIO_S3_URL,
+          s3ForcePathStyle: true,
+        }
+      : {}),
   });
 
   constructor(
