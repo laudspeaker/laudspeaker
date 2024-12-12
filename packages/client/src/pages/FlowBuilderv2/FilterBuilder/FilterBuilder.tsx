2f2137e0c97e6153352eeca6c9fde2245cd9f290
Mahamad Charawi <mahamad@laudspeaker.com>
2024-12-12 09:25:25 -0800

    Fixing multisplit modal pt 2.

--- packages/client/src/pages/FlowBuilderv2/FilterBuilder/FilterBuilder.tsx
+++ packages/client/src/pages/FlowBuilderv2/FilterBuilder/FilterBuilder.tsx
@@ -412,12 +412,16 @@ const FilterBuilder: FC<FilterBuilderProps> = ({
   };
 
   useEffect(() => {
-    loadKeyTypes();
+    // loadKeyTypes();
     loadPossibleKeys();
     loadKeyParameters();
     loadSegments();
     loadPossibleJourneys();
   }, []);
+
+  useEffect(() => {
+    loadKeyTypes();
+  }, [changesHappenIndex]);
 
   useDebounce(
     () => {
