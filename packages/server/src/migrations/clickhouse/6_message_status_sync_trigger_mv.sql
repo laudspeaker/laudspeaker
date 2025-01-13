c3618b032e112d1e975d75d0f4b73ae7cc06f97f
Moustafa Eid <568368+MoustafaEid@users.noreply.github.com>
2025-01-12 20:10:17 -0800

    convert clickhouse migration to typeorm

--- packages/server/src/migrations/clickhouse/6_message_status_sync_trigger_mv.sql
+++
@@ -1,6 +0,0 @@
--- DROP TABLE IF EXISTS event_sync_trigger;
--- CREATE MATERIALIZED VIEW message_status_sync_trigger TO message_status_pg_sync
--- AS SELECT
---     now64() as pg_sync_published_at,
---     *
--- FROM message_status;
